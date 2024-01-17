576cf0490d2a220ca9258f627c060eb555018427
jose.sandoval.tirado <jose.sandoval.tirado@bbva.com>
2024-01-17 15:48:29 -0500

    feat (PERAR-25481): modificar método para obtener información de cliente

--- PISDR601IMPL/src/main/java/com/bbva/pisd/lib/r601/dao/OracleQuotationDAO.java
+++ PISDR601IMPL/src/main/java/com/bbva/pisd/lib/r601/dao/OracleQuotationDAO.java
@@ -2,12 +2,12 @@ package com.bbva.pisd.lib.r601.dao;
 
 import com.bbva.apx.exception.business.BusinessException;
 import com.bbva.pisd.dto.insurancedao.constants.PISDInsuranceErrors;
-import com.bbva.pisd.dto.insurancedao.entities.QuotationEntity;
+import com.bbva.pisd.dto.insurancedao.join.QuotationJoinCustomerInformationDTO;
 import com.bbva.pisd.dto.insurancedao.operation.Operation;
 import com.bbva.pisd.dto.insurancedao.operation.OperationConstants;
 import com.bbva.pisd.lib.r601.interfaces.QuotationDAO;
-import com.bbva.pisd.lib.r601.transfor.bean.QuotationTransforBean;
-import com.bbva.pisd.lib.r601.transfor.map.QuotationTransforMap;
+import com.bbva.pisd.lib.r601.transform.bean.QuotationTransforBean;
+import com.bbva.pisd.lib.r601.transform.map.QuotationTransforMap;
 import com.bbva.pisd.lib.r601.util.FunctionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +20,7 @@ public class OracleQuotationDAO implements QuotationDAO {
     private BaseDAO baseDAO;
 
     @Override
-    public QuotationEntity findQuotationByPolicyQuotaInternalId(String policyQuotaInternalId) {
+    public QuotationJoinCustomerInformationDTO findQuotationByPolicyQuotaInternalId(String policyQuotaInternalId) {
         Map<String,Object> mapQuerySearchByPolicyQuotaInternalId = QuotationTransforMap.policyQuotaInternalIdTransforMap(policyQuotaInternalId);
         if (FunctionUtils.parametersIsValid(mapQuerySearchByPolicyQuotaInternalId,"POLICY_QUOTA_INTERNAL_ID")) {
             Operation operationDTO = Operation.Builder.an().withTypeOperation(OperationConstants.Operation.SELECT)
