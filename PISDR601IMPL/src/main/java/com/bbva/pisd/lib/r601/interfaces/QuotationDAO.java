4988fbd46cac9281db7157343427dad3ac82aaa7
Aaron <carlos.carrillo.delgado@bbva.com>
2024-05-20 19:37:21 -0500

    feat: PARSRBVD-1915 PARSRBVD-3124 New method to find quotation detail from pre emision

--- PISDR601IMPL/src/main/java/com/bbva/pisd/lib/r601/interfaces/QuotationDAO.java
+++ PISDR601IMPL/src/main/java/com/bbva/pisd/lib/r601/interfaces/QuotationDAO.java
@@ -2,8 +2,11 @@ package com.bbva.pisd.lib.r601.interfaces;
 
 import com.bbva.pisd.dto.insurancedao.entities.QuotationEntity;
 
+import java.util.Map;
+
 public interface QuotationDAO {
 
-    QuotationEntity findQuotationByReferenceAndPayrollId(String policyQuotaInternalId) ;
+    QuotationEntity findQuotationByReferenceAndPayrollId(String policyQuotaInternalId);
+    Map<String,Object> findQuotationDetail(String quotationId);
 
 }
