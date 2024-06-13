a59057e091e0bd489cbd8c089cfba789209b7e6f
paulkyev <paul.saire.contractor@bbva.com>
2024-06-13 10:23:47 -0500

    PARSRBVD-1986 PARSRBVD-3441: fix: new build

--- PISDR601/src/main/java/com/bbva/pisd/lib/r601/PISDR601.java
+++ PISDR601/src/main/java/com/bbva/pisd/lib/r601/PISDR601.java
@@ -4,14 +4,8 @@ package com.bbva.pisd.lib.r601;
 
 import java.util.Map;
 
-/**
- * The  interface PISDR601 class...
- */
 public interface PISDR601 {
 
-	/**
-	 * The execute method...
-	 */
 	Map<String,Object> executeFindQuotationByReferenceAndPayrollId(String policyQuotaInternalId);
 	Map<String,Object> executeFindQuotationDetailForPreEmision(String quotationId);
 }
